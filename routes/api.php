9c666b1d2e01eb8d019c659cc316fac53f52e4bc
failamir <fail.amir@students.amikom.ac.id>
2022-08-08 13:36:07 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,42 @@
 <?php
 
-Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin'], function () {
+// Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin'], function () {
+//     Route::post('beli', 'PendaftarController@beliApi')->name('beliApi');
+//     Route::post('notification', 'PendaftarController@notification')->name('notification');
+//     Route::post('scan', 'PendaftarController@scan')->name('scan');
+//     Route::post('checkin', 'PendaftarController@checkin')->name('checkin');
+//     Route::post('checkin2', 'PendaftarController@checkin2')->name('checkin2');
+//     Route::post('checkout', 'PendaftarController@checkout')->name('checkout');
+    
+//     Route::get('list_checkin', 'PendaftarController@list_checkin');
+//     Route::get('list_checkout', 'PendaftarController@list_checkout');
+
+//     Route::post('banners/media', 'BannerApiController@storeMedia')->name('banners.storeMedia');
+//     Route::apiResource('banners', 'BannerApiController');
+
+//     Route::post('transaksis/media', 'TransaksiApiController@storeMedia')->name('transaksis.storeMedia');
+//     Route::apiResource('transaksis', 'TransaksiApiController');
+
+//     Route::post('sponsors/media', 'SponsorApiController@storeMedia')->name('sponsors.storeMedia');
+//     Route::apiResource('sponsors', 'SponsorApiController');
+
+//     Route::apiResource('settings', 'SettingApiController');
+// });
+
+// Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
+    Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin'], function () {
+
     Route::post('beli', 'PendaftarController@beliApi')->name('beliApi');
     Route::post('notification', 'PendaftarController@notification')->name('notification');
     Route::post('scan', 'PendaftarController@scan')->name('scan');
     Route::post('checkin', 'PendaftarController@checkin')->name('checkin');
     Route::post('checkin2', 'PendaftarController@checkin2')->name('checkin2');
     Route::post('checkout', 'PendaftarController@checkout')->name('checkout');
+    Route::post('pendaftars/media', 'PendaftarApiController@storeMedia')->name('pendaftars.storeMedia');
+    Route::apiResource('pendaftars', 'PendaftarApiController');
     
     Route::get('list_checkin', 'PendaftarController@list_checkin');
     Route::get('list_checkout', 'PendaftarController@list_checkout');
-
-    Route::post('banners/media', 'BannerApiController@storeMedia')->name('banners.storeMedia');
-    Route::apiResource('banners', 'BannerApiController');
-
-    Route::post('transaksis/media', 'TransaksiApiController@storeMedia')->name('transaksis.storeMedia');
-    Route::apiResource('transaksis', 'TransaksiApiController');
-
-    Route::post('sponsors/media', 'SponsorApiController@storeMedia')->name('sponsors.storeMedia');
-    Route::apiResource('sponsors', 'SponsorApiController');
-
-    Route::apiResource('settings', 'SettingApiController');
-});
-
-Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
     // Tiket
     Route::post('tikets/media', 'TiketApiController@storeMedia')->name('tikets.storeMedia');
     Route::apiResource('tikets', 'TiketApiController');
