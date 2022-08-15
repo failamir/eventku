fa7aff62db7b9e49ae86602db8116876d0eb1630
failamir <fail.amir@students.amikom.ac.id>
2022-08-16 02:26:05 +0700

    62e980b6dd01062e980b6dd012

--- app/Http/Controllers/Api/V1/Admin/ApiController.php
+++ app/Http/Controllers/Api/V1/Admin/ApiController.php
@@ -229,7 +229,9 @@ class ApiController extends Controller
 
     public function scanqr(Request $request)
     {
-        $pendaftar = Tiket::where('qr', $request->input('qr'))->orWhere('email', $request->input('qr'))->first();
+        $pendaftar = Tiket::where('qr', $request->input('qr'))
+        // ->orWhere('email', $request->input('qr'))
+        ->first();
         if (empty($pendaftar)) {
             $snap = new stdClass();
             $snap->data = 'QR not Found';
