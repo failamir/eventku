d859b684ac3b0a81113d6b244c54f029170ee954
failamir <fail.amir@students.amikom.ac.id>
2022-08-15 23:27:58 +0700

    generate

--- app/Http/Controllers/Api/V1/Admin/ApiController.php
+++ app/Http/Controllers/Api/V1/Admin/ApiController.php
@@ -229,7 +229,7 @@ class ApiController extends Controller
         }
         $snap = new stdClass();
         if ($pendaftar->checkin == null) $pendaftar->checkin = 'belum';
-        if ($pendaftar->no_tiket == null) $pendaftar->no_tiket = 'generate';
+        if ($pendaftar->no_tiket == 'generate') $pendaftar->no_tiket = '';
         $snap->code = $request->input('qr');
         $snap->checkin = $pendaftar->checkin;
         $snap->no_tiket = $pendaftar->no_tiket;
