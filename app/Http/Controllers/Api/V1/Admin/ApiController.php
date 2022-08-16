efd287fcd708522983eb1f2ef6bc0ab44d252848
failamir <fail.amir@students.amikom.ac.id>
2022-08-17 01:59:47 +0700

    Update ApiController.php

--- app/Http/Controllers/Api/V1/Admin/ApiController.php
+++ app/Http/Controllers/Api/V1/Admin/ApiController.php
@@ -143,7 +143,7 @@ class ApiController extends Controller
         // abort_if ( Gate::denies( 'pendaftar_access' ), Response::HTTP_FORBIDDEN, '403 Forbidden' );
         $s = $_GET['uid'];
         // if ($s == 'sudahdanterpakai') {
-        $data = Tiket::with(['event'])->where('pic_checkin', $s)->orWhere('checkin', 'sudah')->orWhere('checkin', 'sudah-note')->orWhere('checkin', 'terpakai')->OrderBy('updated_at', 'ASC')->limit(20)->get();
+        $data = TiketQR::with(['event'])->where('pic_checkin', $s)->orWhere('checkin', 'sudah')->orWhere('checkin', 'sudah-note')->orWhere('checkin', 'terpakai')->OrderBy('updated_at', 'ASC')->limit(20)->get();
         // else {return new UserResource(Tiket::with(['event'])->where('checkin',$s)->OrderBy('updated_at','ASC')->paginate(10));}
 
         $snap = new stdClass();
