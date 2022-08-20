df089aa43c2073af5e62cdd0af25dfad437ab2e4
failamir <fail.amir@students.amikom.ac.id>
2022-08-20 09:08:19 +0700

    Update ApiController.php

--- app/Http/Controllers/Api/V1/Admin/ApiController.php
+++ app/Http/Controllers/Api/V1/Admin/ApiController.php
@@ -274,6 +274,12 @@ class ApiController extends Controller
             $snap->data = 'Tiket not Found';
             return response(json_encode($snap), Response::HTTP_FORBIDDEN);
         }
+        $pendaftar = TiketQR::where('no_tiket', $request->input('no_tiket'))->first();
+        if (empty($pendaftar)) {
+            $snap = new stdClass();
+            $snap->data = 'Tiket sudah di assign';
+            return response(json_encode($snap), Response::HTTP_FORBIDDEN);
+        }
         $tiket = Tiket::where('no_tiket', $request->input('no_tiket'))->first();
         $pendaftar->update(['nama' =>  $tiket->nama, 'peserta_id' =>  $tiket->peserta_id, 'event_id' =>  $tiket->event_id, 'checkin' =>  'belum', 'status' =>  'assign', 'no_tiket' => $request->input('no_tiket'), 'pic_assign' => $request->input('uid')]);
         // $pendaftar = Tiket::where('no_tiket', $request->input('no_tiket'))->first();
