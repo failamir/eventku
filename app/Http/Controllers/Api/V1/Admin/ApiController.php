df3f7b508ba757606fe89134ef0b67decca21c9c
failamir <fail.amir@students.amikom.ac.id>
2022-08-22 01:58:33 +0700

    Update ApiController.php

--- app/Http/Controllers/Api/V1/Admin/ApiController.php
+++ app/Http/Controllers/Api/V1/Admin/ApiController.php
@@ -190,11 +190,14 @@ class ApiController extends Controller
         $tanggal_mulai = Event::find($pendaftar->event_id)->tanggal_mulai;
         
         $tanggal_selesai = Event::find($pendaftar->event_id)->tanggal_selesai;
-            // var_dump($tanggal_mulai);
-            // var_dump($tanggal_selesai);
-            // var_dump(date('Y-m-d'));
-            // $datenow = date('Y-m-d');
-            $datenow = date('Y-m-d', strtotime('+1 days'));
+            
+            $datenow = date('Y-m-d', strtotime('+1 days')); 
+
+            var_dump(date('Y-m-d'));
+            var_dump(date('Y-m-d', strtotime('+1 days')));
+            var_dump($tanggal_mulai);
+            var_dump($tanggal_selesai);
+
         if ( $tanggal_mulai < $datenow) {
             $snap = new stdClass();
             $snap->code = $request->input('qr');
