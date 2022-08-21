8228af897a5f3ec5ee42f59879079a4e29ca1deb
failamir <fail.amir@students.amikom.ac.id>
2022-08-22 01:26:24 +0700

    Update ApiController.php

--- app/Http/Controllers/Api/V1/Admin/ApiController.php
+++ app/Http/Controllers/Api/V1/Admin/ApiController.php
@@ -180,8 +180,8 @@ class ApiController extends Controller
 
     public function qrcheckin(Request $request)
     {
-        // $pendaftar = TiketQR::where('email', $request->input('qr'))->first();
-        $pendaftar = Tiket::where('email', $request->input('qr'))->orWhere('qr', $request->input('qr'))->where('no_tiket', '!=', 'generate')->withTrashed()->first();
+        // $pendaftar = Tiket::where('email', $request->input('qr'))->first();
+        $pendaftar = TiketQR::where('qr', $request->input('qr'))->orWhere('email', $request->input('qr'))->where('no_tiket', '!=', 'generate')->withTrashed()->first();
         if(empty($pendaftar)){
             return Response::json([
                 'data' => 'QR tidak ditemukan'
